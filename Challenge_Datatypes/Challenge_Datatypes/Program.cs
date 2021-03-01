6afc60c12e103ef370c78cd4bd81e234b74c5b49
Logan Potts <pottld01@gmail.com>
2021-03-01 12:21:29 -0500

    adding datatypes challenge

---
+++ Challenge_Datatypes/Challenge_Datatypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Challenge_Datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte bitty = 115;
+            short shorty = 30000;
+            int intty = 1000000000;
+            long longy = 1000000000000;
+
+            float floaty = 10.55467F;
+            double doubly = 11.11567453;
+            decimal deccy = 12.14784378378473843874M;
+
+            bool booly = true;
+            char charry = 'J';
+
+            string stringy = "I control text";
+            string stringyNum = "48";
+            int intyNum = Int32.Parse(stringyNum);
+            Console.WriteLine(intyNum);
+        }
+    }
+}
